ef5f7da056581cb1d48697fb2388be0af9ecb519
Poojitha-Ramya <chpujitaramya@example.com>
2026-05-26 15:21:58 +0530

    Fixed TanStack router import

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={getRouter()} />
     </React.StrictMode>
 );
